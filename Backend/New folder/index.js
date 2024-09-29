112ade81e52608ec3d8aaae32f54230ea105a391
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-09-29 15:41:42 +0530

    Add files via upload

--- Backend/New folder/index.js
+++ Backend/New folder/index.js
@@ -1,8 +1,33 @@
 const fs = require('node:fs');
 
 
-fs.writeFile("abcd.txt", "hey my name is shiv kumar ", function (err) {
-    if (err) { console.log("err") }
-    else { console.log("file created") }
+// fs.writeFile("abcd.txt", "hey my name is shiv kumar ", function (err) {
+//     if (err) { console.log("err") }
+//     else { console.log("file created") }
+
+// })
+
+
+// read file in node js 
+// fs.readFile("abcd.txt", "utf8", function (err, data) {
+
+//     if (err) { console.log("err") }
+//     else { console.log(data) }
+// })
+
+//append in a file 
+// fs.appendFile("abcd.txt", "        this data is append ", function (err) {
+//     if (err) {
+//         console.log("err")
+
+//     }
+//     else { console.log("data append safely") }
+
+// })
+// rename a file 
+
+fs.rename("new.txt", "new1.txt", function (err) {
+    if (err) { console.log("an error ") }
+    else { console.log("file renamed") }
 
 })
