165a3337d11ce812479fd83b6fa5e9cf374346c1
Shiv Kumar <140259236+shiv90154@users.noreply.github.com>
2024-09-30 06:04:02 +0530

    Add files via upload

---
+++ Backend/day2/index.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+
+// fs.writeFileSync("hello.txt", "my name is shiv kumar this is day 2",);
+// console.log("shiv kumar")
+
+// fs.readFileSync("hello.txt", String)
+// console.log("shiv kumar 2")
+
+
+// fs.appendFileSync("hello.txt", "    jai shri ram          jai shri ram ")
+
+fs.chmodSync("hello.txt", String)
+console.log("shiv kumar")
